13ecdbb09c7a76eb3f0687d518993ba24d2595ef
Xavier <cxav12@gmail.com>
2026-08-17 00:28:27 -0400

    Tighten footer spacing

--- yankees/contact/index.php
+++ yankees/contact/index.php
@@ -33,7 +33,7 @@ $notice = $messages[$status] ?? null;
     <base href="/yankees/contact/" />
     <title>Contact | Yankees Fan Site</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="../shared.css?v=20260817-contact-center1" />
+    <link rel="stylesheet" href="../shared.css?v=20260817-compact-footer1" />
   </head>
   <body>
     <main class="container-fluid app-shell">

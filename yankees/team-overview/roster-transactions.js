8296db1227a7837e77f8cabcf158dbc343b67215
Xavier <cxav12@gmail.com>
2026-09-06 00:49:52 -0400

    match the roster card styling

--- yankees/team-overview/roster-transactions.js
+++ yankees/team-overview/roster-transactions.js
@@ -341,7 +341,7 @@ function coachCard(entry) {
   const article = document.createElement("article");
   article.className = "coach-card";
   const personId = Number(entry.person?.id);
-  const portrait = `https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:83:current.png/ar_1:1,c_pad,b_auto:border/r_max/w_180,q_auto:best/v1/people/${personId}/headshot/83/coach/current`;
+  const portrait = `https://img.mlbstatic.com/mlb-photos/image/upload/w_180,q_auto:best,f_png/v1/people/${personId}/headshot/83/coach/current`;
   const copy = document.createElement("div");
   copy.className = "coach-card-copy";
   const heading = document.createElement("span");
